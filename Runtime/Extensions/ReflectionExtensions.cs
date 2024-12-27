d27d075287a57971065e9669ac8f119d7809ffb4
Vaclav Bily <xbily@fi.muni.cz>
2024-12-27 19:09:06 +0100

    Added extensions for reflection - finding classes of type

---
+++ Runtime/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xprees.EditorTools.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        /// Returns all implementations of the interface or class
+        public static List<T> GetImplementations<T>()
+        {
+            var type = typeof(T);
+            var types = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract)
+                .ToList();
+
+            return types
+                .Select(Activator.CreateInstance)
+                .Cast<T>()
+                .ToList();
+        }
+    }
+}
