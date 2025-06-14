647899c5d4d74684c97c74c12b46bfb303188fc0
Vaclav Bily <xbily@fi.muni.cz>
2025-06-15 00:19:35 +0200

    ReflectionExtensions - added Unity Editor optimization using TypeCache

--- Runtime/Extensions/ReflectionExtensions.cs
+++ Runtime/Extensions/ReflectionExtensions.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEditor;
+using ZLinq;
 
 namespace Xprees.EditorTools.Extensions
 {
     public static class ReflectionExtensions
     {
         /// Returns all implementations of the interface or class
+        /// Supports both Unity Editor and runtime environments. Might not work in AOT builds.
         public static List<T> GetImplementations<T>()
         {
+#if UNITY_EDITOR
+            // TypeCache is much faster than AppDomain.GetAssemblies() in Unity Editor
+            var types = TypeCache
+                .GetTypesDerivedFrom<T>()
+                .AsValueEnumerable();
+#else
             var type = typeof(T);
-            var types = AppDomain.CurrentDomain
-                .GetAssemblies()
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .AsValueEnumerable()
                 .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract)
-                .ToList();
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract);
+#endif
 
             return types
                 .Select(Activator.CreateInstance)
