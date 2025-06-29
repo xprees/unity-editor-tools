c7153a6777dd35ba2020aff970da1f212620b1ed
Vaclav Bily <xbily@fi.muni.cz>
2025-06-30 01:09:24 +0200

    Add DomainReloader utility to reload Unity domain from editor menu

---
+++ Editor/Menu/DomainReloader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Xprees.EditorTools.Editor.Menu
+{
+
+    public static class DomainReloader
+    {
+        /// This method is used to reload the Unity domain, which is useful when you want to apply changes made to scripts without restarting the editor.
+        [MenuItem("Tools/Reload Domain")]
+        public static void ReloadDomain()
+        {
+            EditorUtility.RequestScriptReload();
+        }
+    }
+}
